428070c11ca76226a3c6e7a403c1e1ae2aed08d3
Erik Billerby <billerby@gmail.com>
2016-09-05 15:20:32 +0200

    elaborate

---
+++ copy-jar.sh
@@ -0,0 +1,1 @@
+cp repo/target/alfresco-archive-toolkit-repo-1.0.0-SNAPSHOT.jar /opt/alfresco/enterprise/4.2.6.1/vgr/tomcat-repo/webapps/alfresco/WEB-INF/lib
---
+++ repo/src/main/java/org/redpill/alfresco/archive/repo/action/executor/ConvertPdfToPdfaActionExecuter.java
@@ -0,0 +1,24 @@
+package org.redpill.alfresco.archive.repo.action.executor;
+
+import java.util.List;
+
+import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+public class ConvertPdfToPdfaActionExecuter extends ActionExecuterAbstractBase {
+
+  @Override
+  protected void executeImpl(Action arg0, NodeRef arg1) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  protected void addParameterDefinitions(List<ParameterDefinition> arg0) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
