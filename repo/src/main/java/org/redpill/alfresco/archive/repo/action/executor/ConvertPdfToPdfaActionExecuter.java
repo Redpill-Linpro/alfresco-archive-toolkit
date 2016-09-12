6299967d5b8a63d21e1478710ff982b1fbf408a1
Erik Billerby <billerby@gmail.com>
2016-09-12 13:31:20 +0200

    under development

--- repo/src/main/java/org/redpill/alfresco/archive/repo/action/executor/ConvertPdfToPdfaActionExecuter.java
+++ repo/src/main/java/org/redpill/alfresco/archive/repo/action/executor/ConvertPdfToPdfaActionExecuter.java
@@ -4,14 +4,22 @@ import java.util.List;
 
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ActionService;
 import org.alfresco.service.cmr.action.ParameterDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 
 public class ConvertPdfToPdfaActionExecuter extends ActionExecuterAbstractBase {
 
+  private ActionService actionService;
+  
+  public void setActionService(ActionService actionService) {
+    this.actionService = actionService;
+  }
+  
   @Override
-  protected void executeImpl(Action arg0, NodeRef arg1) {
-    // TODO Auto-generated method stub
+  protected void executeImpl(Action action, NodeRef nodeRef) {
+    Action createPdf = actionService.createAction(CreatePdfActionExecuter.NAME);
+    createPdf.setParameterValue(CreatePdfActionExecuter.PARAM_MIME_TYPE, "application/pdfa");
 
   }
 
