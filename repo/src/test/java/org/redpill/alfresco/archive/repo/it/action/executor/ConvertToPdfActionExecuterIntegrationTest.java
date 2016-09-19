6eba389cdf69689169c6f7cc2e005d009a256ce6
Erik Billerby <billerby@gmail.com>
2016-09-19 13:53:12 +0200

    test methods

--- repo/src/test/java/org/redpill/alfresco/archive/repo/it/action/executor/ConvertToPdfActionExecuterIntegrationTest.java
+++ repo/src/test/java/org/redpill/alfresco/archive/repo/it/action/executor/ConvertToPdfActionExecuterIntegrationTest.java
@@ -1,5 +1,6 @@
 package org.redpill.alfresco.archive.repo.it.action.executor;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.model.RenditionModel;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.service.cmr.action.Action;
@@ -14,8 +15,11 @@ import org.redpill.alfresco.test.AbstractRepoIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import  org.junit.Assert;
+
 public class ConvertToPdfActionExecuterIntegrationTest extends AbstractRepoIntegrationTest{
   private static final String RENDITION_NAME_PDF = "pdf";
+  private static final String RENDITION_NAME_PDFA = "pdfa";
 
   private static final String DEFAULT_USERNAME = "testuser_" + System.currentTimeMillis();
 
@@ -36,30 +40,9 @@ public class ConvertToPdfActionExecuterIntegrationTest extends AbstractRepoInteg
     site = createSite();
   }
 
-  /**
-   *               <param name="function">onActionSimpleRepoAction</param>
-              <param name="action">archive-toolkit-transform-to-pdf</param>
-              <param name="mime-type">application/pdf</param>
-              <param name="destination-folder">{nodeRef}</param>
-              <param name="assoc-type">rn:rendition</param>
-              <param name="assoc-name">cm:pdf</param>
-              <param name="target-name">pdf</param>
-              <param name="overwrite-copy">false</param>
-              <param name="successMessage">message.archive-toolkit-convert-to-pdf-rendition.success</param>
-              <param name="failureMessage">message.archive-toolkit-convert-to-pdf-rendition.failure</param>
-              
-               public static final String PARAM_MIME_TYPE = "mime-type";
-  public static final String PARAM_DESTINATION_FOLDER = "destination-folder";
-  public static final String PARAM_ASSOC_TYPE_QNAME = "assoc-type";
-  public static final String PARAM_ASSOC_QNAME = "assoc-name";
-  public static final String PARAM_TARGET_NAME = "target-name";
-  public static final String PARAM_OVERWRITE_COPY = "overwrite-copy";
-
-  public static final String FAKE_MIMETYPE_PDFA = "application/pdfa";
-
-   */
-  @Test
-  public void testConvertToPdf() throws InterruptedException {
+  
+  //@Test
+  public void testConvertDocxToPdf() throws InterruptedException {
     NodeRef document = uploadDocument(site, "test.docx").getNodeRef();
     
     Action action = actionService.createAction(ConvertToPdfActionExecuter.NAME);
@@ -72,6 +55,27 @@ public class ConvertToPdfActionExecuterIntegrationTest extends AbstractRepoInteg
     action.setParameterValue(ConvertToPdfActionExecuter.PARAM_OVERWRITE_COPY, false);
     
     actionService.executeAction(action, document);
+  }
+  
+  @Test
+  public void testConvertPdfToPdfa() throws InterruptedException {
+    NodeRef document = uploadDocument(site, "test.pdf").getNodeRef();
+    
+    Action action = actionService.createAction(ConvertToPdfActionExecuter.NAME);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_MIME_TYPE, ConvertToPdfActionExecuter.FAKE_MIMETYPE_PDFA);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_DESTINATION_FOLDER, document);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_ASSOC_TYPE_QNAME, RenditionModel.ASSOC_RENDITION);
+    QName renditionQName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, (String) RENDITION_NAME_PDFA);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_ASSOC_QNAME, renditionQName);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_TARGET_NAME, RENDITION_NAME_PDFA);
+    action.setParameterValue(ConvertToPdfActionExecuter.PARAM_OVERWRITE_COPY, false);
+    
+    actionService.executeAction(action, document);
+    
+    //Assert that there is a child node with name pdfa
+    NodeRef renditionedNode = _nodeService.getChildByName(document, RenditionModel.ASSOC_RENDITION, RENDITION_NAME_PDFA);
+    
+    //Assert.assertEquals(RENDITION_NAME_PDFA, (String)_nodeService.getProperty(renditionedNode, ContentModel.PROP_NAME));
   }
   @Override
   public void afterClassSetup() {
