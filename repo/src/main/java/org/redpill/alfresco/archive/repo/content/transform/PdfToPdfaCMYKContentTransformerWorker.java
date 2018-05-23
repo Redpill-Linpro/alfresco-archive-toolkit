a8ac1ced74d36f840e74e6b2cfd9108a49d54f9a
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2018-05-23 07:31:36 +0200

    Refactoring

--- repo/src/main/java/org/redpill/alfresco/archive/repo/content/transform/PdfToPdfaCMYKContentTransformerWorker.java
+++ repo/src/main/java/org/redpill/alfresco/archive/repo/content/transform/PdfToPdfaCMYKContentTransformerWorker.java
@@ -1,30 +1,27 @@
 package org.redpill.alfresco.archive.repo.content.transform;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.AccessDeniedException;
-import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.repo.content.MimetypeMap;
-import org.alfresco.repo.content.transform.RuntimeExecutableContentTransformerWorker;
 import org.alfresco.service.cmr.repository.TransformationOptions;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.redpill.alfresco.archive.repo.action.executor.ConvertToPdfActionExecuter;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.Assert;
 
-public class PdfToPdfaCMYKContentTransformerWorker extends PdfToPdfaContentTransformerWorker implements InitializingBean {
+public class PdfToPdfaCMYKContentTransformerWorker extends AbstractPdfToPdfaContentTranformer {
 
   private static final Log logger = LogFactory.getLog(PdfToPdfaCMYKContentTransformerWorker.class);
 
-  protected String pdfaDefinitionFile;
-  protected String pdfaICCProfileFile;
-  protected static final String PDFA_DEFINITION_TEMPLATE_PATH = "alfresco/module/archive-toolkit/gs/PDFA_def2.ps";
+  protected static final String PDFA_DEFINITION_TEMPLATE_PATH = "alfresco/module/archive-toolkit/gs/PDFA_CMYK_def.ps";
   protected static final String PDFA_ICCPROFILE_PATH = "alfresco/module/archive-toolkit/gs/CoatedGRACoL2006.icc";
+
+  @Override
+  protected String getPdfaDefinitionTemplatePath() {
+    return PDFA_DEFINITION_TEMPLATE_PATH;
+  }
+
+  @Override
+  protected String getPdfaIccProfilePath() {
+    return PDFA_ICCPROFILE_PATH;
+  }
 
   @Override
   public boolean isTransformable(String sourceMimetype, String targetMimetype, TransformationOptions options) {
