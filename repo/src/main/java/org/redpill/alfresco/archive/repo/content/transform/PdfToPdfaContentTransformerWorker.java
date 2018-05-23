b6615dc553ec0b7dc2153647af5e9f482771dbee
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2018-05-23 09:01:26 +0200

    Cleanup

--- repo/src/main/java/org/redpill/alfresco/archive/repo/content/transform/PdfToPdfaContentTransformerWorker.java
+++ repo/src/main/java/org/redpill/alfresco/archive/repo/content/transform/PdfToPdfaContentTransformerWorker.java
@@ -1,21 +1,12 @@
 package org.redpill.alfresco.archive.repo.content.transform;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.AccessDeniedException;
-import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.repo.content.transform.RuntimeExecutableContentTransformerWorker;
 import org.alfresco.service.cmr.repository.TransformationOptions;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.redpill.alfresco.archive.repo.action.executor.ConvertToPdfActionExecuter;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.Assert;
 
 public class PdfToPdfaContentTransformerWorker extends RuntimeExecutableContentTransformerWorker implements InitializingBean {
 
--- repo/src/main/java/org/redpill/alfresco/archive/repo/model/ArchiveToolkitModel.java
+++ repo/src/main/java/org/redpill/alfresco/archive/repo/model/ArchiveToolkitModel.java
@@ -5,10 +5,10 @@ import org.alfresco.service.namespace.QName;
 public interface ArchiveToolkitModel {
 
   static final String URI = "http://www.redpill-linpro.com/model/at/1.0";
-  
+
   static final QName ASPECT_CHECKSUMMED = QName.createQName(URI, "checksummed");
   static final QName PROP_CHECKSUM = QName.createQName(URI, "checksum");
-  
+
   static final String CHECKSUM_MD5 = "md5";
 
   static final String CHECKSUM_SHA1 = "sha1";
