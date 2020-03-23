3f85eb247e151ff5501befd9f2773ddf5b5d2749
Filip Labe <filip@redpill-linpro.com>
2020-03-23 08:11:32 +0100

    Updated pdf2pdfa with timeout

--- pdf2pdfa/pdf2pdfa-transform.sh
+++ pdf2pdfa/pdf2pdfa-transform.sh
@@ -5,6 +5,7 @@ SOFFICE_USER=$2
 INPUT_FILE=$3
 OUTPUT_FILE=$4
 TEMP_DIR=$SOFFICE_USER/transformtmp
+TIMEOUT_VAR=${PDF_TRANSFORM_TIMEOUT:-60}
 
 USAGE="Usage: pdf2pdfa-transform.sh /path/to/soffice /path/to/soffice/user /path/to/inputfile.pdf /path/to/outputfile.pdf"
 PDFA_CONFIG="<item oor:path=\"/org.openoffice.Office.Common/Filter/PDF/Export\"><prop oor:name=\"SelectPdfVersion\" oor:op=\"fuse\"><value>1</value></prop></item>"
@@ -37,21 +38,25 @@ TMP_FILENAME=$(basename "$INPUT_FILE")
 OUTPUT_FILE_NAME=${TMP_FILENAME%.*}.pdf
 SED="sed"
 TIMEOUT="timeout"
-$SOFFICE_BIN --headless "-env:UserInstallation=file://$SOFFICE_USER" --convert-to pdf --outdir "$TEMP_DIR" "$INPUT_FILE"
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  # Mac OSX - sed does not work properly on macosx, use gnused
+  SED="gsed"
+  TIMEOUT="gtimeout"
+fi
 
-if grep -q SelectPdfVersion "$SOFFICE_USER/user/registrymodifications.xcu"; then
-  echo "Created pdf/a from pdf (1)"
+if grep -q SelectPdfVersion "$SOFFICE_USER/user/registrymodifications.xcu";
+then
+  echo "Tool already configured"
 else
   echo "Configuring tool to produce pdf/a"
-  if [[ "$OSTYPE" == "darwin"* ]]
-  then
-    # Mac OSX - sed does not work properly on macosx, use gnused
-    SED="gsed"
-    TIMEOUT="gtimeout"
-  fi
-  $SED -i "3i$PDFA_CONFIG" $SOFFICE_USER/user/registrymodifications.xcu
-  $TIMEOUT 60 $SOFFICE_BIN --headless "-env:UserInstallation=file://$SOFFICE_USER" --convert-to pdf --outdir "$TEMP_DIR" "$INPUT_FILE"
-  echo "Created pdf/a from pdf (2)"
+  $SED -i "3i$PDFA_CONFIG" "$SOFFICE_USER/user/registrymodifications.xcu"
+fi
+
+if ! $TIMEOUT --preserve-status  --signal 9 "$TIMEOUT_VAR" "$SOFFICE_BIN" --headless "-env:UserInstallation=file://$SOFFICE_USER" --convert-to pdf --outdir "$TEMP_DIR" "$INPUT_FILE"
+then echo "Timed out creating pdf/a from pdf" ;
+exit 1
+else echo "Created pdf/a from pdf" ;
 fi
 
 echo "Copying $TEMP_DIR/$OUTPUT_FILE_NAME to $OUTPUT_FILE"
